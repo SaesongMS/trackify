7423343782f69dfd4d52cea350668db0c427a755
SaesongMS <saesongms1@gmail.com>
2023-11-18 20:12:09 +0100

    Modyfikacja logiki i stylowanie wyszukiwania

---
+++ frontend/src/components/searchpage/searchgrid.jsx
@@ -0,0 +1,34 @@
+import SearchCard from "./searchcard";
+
+function SearchGrid(props) {
+    const { subjects, type, ...others } = props;
+
+    const capitalizedType = type.charAt(0).toUpperCase() + type.slice(1);
+
+    const renderCard = (subject) => {
+        switch(capitalizedType){
+            case "Artist":
+                return <SearchCard photo={subject.photo} name={subject.name} subject="artist" />
+            case "Album":
+                return <SearchCard photo={subject.cover} name={subject.name} subject="album" />
+            case "Song":
+                return <SearchCard photo={subject.album.cover} name={subject.title} subject="song" />
+            case "User":
+                return <SearchCard photo={subject.profilePicture} name={subject.userName} subject="user" />
+        }
+    }
+
+    return (
+    <>
+        <p class="text-2xl mt-5 mb-5">{capitalizedType}</p>
+        <div class="w-full xl:w-[80%] mx-auto xl:mx-0">
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-2">
+            {subjects.map((subject) => (
+                renderCard(subject)
+            ))}
+            </div>
+        </div>
+    </>
+    );
+}
+export default SearchGrid;
