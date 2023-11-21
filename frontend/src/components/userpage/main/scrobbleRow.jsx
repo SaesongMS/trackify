88bf7b7d996f0516d441bad81dac64517aee1aab
Dawid Siwak <dawidsiwak2137@gmail.com>
2023-11-21 01:23:43 +0100

    scrobblerow change

--- frontend/src/components/userpage/main/scrobbleRow.jsx
+++ frontend/src/components/userpage/main/scrobbleRow.jsx
@@ -27,7 +27,8 @@ function ScrobbleRow(props) {
   }
 
   return (
-    <div class="text-[#f2f3ea] flex flex-row w-[100%] pl-3 border border-[#3f4147] items-center hover:rounded-sm hover:border-slate-500 transition-all duration-150 h-[10%]">
+    <div class="text-[#f2f3ea] h-[100%] flex flex-row w-[100%] border border-[#3f4147] items-center hover:rounded-sm hover:border-slate-500 transition-all duration-150">
+      
       <img
         class="mr-4 cursor-pointer max-w-[10%] hover:opacity-80 transition-all duration-150"
         src={`data:image/png;base64,${albumCover}`}
@@ -35,22 +36,29 @@ function ScrobbleRow(props) {
           (window.location.href = `/song/${title.replaceAll(" ", "+")}`)
         }
       />
-      <span class="mr-4 cursor-pointer max-w-[10%]">
+      <div class="flex flex-grow">
+
+      <span class="mr-4 cursor-pointer max-w-[10%] flex flex-grow">
         <img src={heart} class="w-4" />
       </span>
-      <div class="flex flex-col md:flex-row max-w-[75%] md:justify-center md:items-center">
         <span class="md:mr-4 cursor-pointer hover:text-slate-300 truncate">
           <A href={`/song/${title.replaceAll(" ", "+")}`}>{title}</A>
         </span>
-        <span class="md:mr-4 cursor-pointer hover:text-slate-300truncate">
+      </div>
+      
+      
+
+      <div class="flex flex-col md:flex-row max-w-[75%] md:justify-between md:items-center">
+        <span class="md:mr-4 cursor-pointer hover:text-slate-300 truncate max-w-[30%]">
           <A href={`/artist/${artist.replaceAll(" ", "+")}`}>{artist}</A>
         </span>
         <span class="md:mr-4 cursor-pointer md:max-w-[10%]">{rating ? rating : ""}</span>
         <span class="md:mr-4 cursor-default md:max-w-[15%]">
           {date ? formatTimeDifference(date) : ""}
         </span>
+      <span class="mr-2 ml-4 cursor-pointer text-red-500 max-w-[5%]">x</span>
       </div>
-      <span class="mr-4 cursor-pointe text-red-500 max-w-[5%]">x</span>
+      
     </div>
   );
 }
