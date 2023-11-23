c9d3df3c18ad97268369e2904c449d5d2b40f1d5
Bobozia <sokoliukbohdan@gmail.com>
2023-11-23 14:47:58 +0100

    Added lacking function to previous fix

--- frontend/src/components/subjectpage/subjectinfobar.jsx
+++ frontend/src/components/subjectpage/subjectinfobar.jsx
@@ -1,51 +1,52 @@
 import Counter from "../userpage/userbanner/counter";
 import { createEffect, createSignal } from "solid-js";
-function SubjectInfoBar(props){
-    const [primaryText, setprimaryText] = createSignal(null);
-    const [secondaryText, setsecondaryText] = createSignal(null);
-    const [scrobbleCount, setscrobbleCount] = createSignal(null);
-    const [usersCount, setusersCount] = createSignal(null);
-    const [image, setimage] = createSignal(null);
-    const [subject, setsubject] = createSignal(null);
+import { encodeSubjectName } from "../../encodeSubjectName";
+function SubjectInfoBar(props) {
+  const [primaryText, setprimaryText] = createSignal(null);
+  const [secondaryText, setsecondaryText] = createSignal(null);
+  const [scrobbleCount, setscrobbleCount] = createSignal(null);
+  const [usersCount, setusersCount] = createSignal(null);
+  const [image, setimage] = createSignal(null);
+  const [subject, setsubject] = createSignal(null);
 
-    createEffect(() => {
-        setprimaryText(props.primaryText);
-        setsecondaryText(props.secondaryText);
-        setscrobbleCount(props.scrobbleCount);
-        setusersCount(props.usersCount);
-        setimage(props.image);
-        setsubject(props.subject);
-        console.log(props);
-        console.log(scrobbleCount());
-        console.log(usersCount());
-    }, [props]);
+  createEffect(() => {
+    setprimaryText(props.primaryText);
+    setsecondaryText(props.secondaryText);
+    setscrobbleCount(props.scrobbleCount);
+    setusersCount(props.usersCount);
+    setimage(props.image);
+    setsubject(props.subject);
+    console.log(props);
+    console.log(scrobbleCount());
+    console.log(usersCount());
+  }, [props]);
 
-    const handleNavigation = () => {
-        if(subject() === "album" || subject() === "song"){
-            window.location.href = `/artist/${secondaryText().replaceAll(
-                " ",
-                "+"
-            )}`
-        }
-        }
-    
-    return(
-      <div class="h-[100%] bg-no-repeat bg-cover" 
-        style={`background-image: url(data:image/png;base64,${image()})`}
+  const handleNavigation = () => {
+    if (subject() === "album" || subject() === "song") {
+      window.location.href = `/artist/${encodeSubjectName(secondaryText())}`;
+    }
+  };
+
+  return (
+    <div
+      class="h-[100%] bg-no-repeat bg-cover"
+      style={`background-image: url(data:image/png;base64,${image()})`}
+    >
+      <div
+        class={`h-[100%] flex flex-col md:flex-row w-[100%] bg-black bg-opacity-50 text-slate-200 pb-2 pl-2`}
       >
-
-        <div class={`h-[100%] flex flex-col md:flex-row w-[100%] bg-black bg-opacity-50 text-slate-200 pb-2 pl-2`}>
-            
-            <div class="flex flex-col justify-end ml-2">
-              <div class="text-[40px] font-bold">{primaryText()}</div>
-              <div onClick={handleNavigation} class="cursor-pointer">{secondaryText()}</div>
-            </div>
-            <div class="flex ml-2 md:ml-12 md:pl-4">
-                <Counter title="Scrobbles" count={scrobbleCount()}/>
-                <Counter title="Listeners" count={usersCount()}/>
-            </div>
+        <div class="flex flex-col justify-end ml-2">
+          <div class="text-[40px] font-bold">{primaryText()}</div>
+          <div onClick={handleNavigation} class="cursor-pointer">
+            {secondaryText()}
           </div>
+        </div>
+        <div class="flex ml-2 md:ml-12 md:pl-4">
+          <Counter title="Scrobbles" count={scrobbleCount()} />
+          <Counter title="Listeners" count={usersCount()} />
+        </div>
       </div>
-    )
+    </div>
+  );
 }
 export default SubjectInfoBar;
